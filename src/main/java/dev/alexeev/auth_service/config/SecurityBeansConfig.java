5611b518bce602558545c36fe472ab4f63179bef
PC <crapiva315@gmail.com>
2026-08-17 14:10:06 +0300

    Add JWT authentication filter, admin-only protection for register/delete endpoints, seed admin user

--- src/main/java/dev/alexeev/auth_service/config/SecurityBeansConfig.java
+++ src/main/java/dev/alexeev/auth_service/config/SecurityBeansConfig.java
@@ -30,7 +30,7 @@ public class SecurityBeansConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
-            .csrf(csrf -> csrf.disable())
+            .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .exceptionHandling(ex -> ex
                     .authenticationEntryPoint((request, response, authException) ->
